8add4663210050f85e0925ff3c6269bbfd0ee159
Jonathan Augustin <tun45218@temple.edu>
2024-04-09 11:31:02 -0400

    Added timerBinder.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,14 +1,34 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var timerBinder : TimerService.TimerBinder
+    var isConnected = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+                timerBinder = p1 as TimerService.TimerBinder
+                isConnected = true
+            }
+
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                isConnected = false
+            }
+
+        }
+
+        //
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
 
