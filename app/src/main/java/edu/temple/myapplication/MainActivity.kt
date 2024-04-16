6411a20bdcdd5e2d392de28f98da801c9c28ac2a
Jonathan Augustin <tun45218@temple.edu>
2024-04-16 11:40:45 -0400

    Added "menuInflater" in MainActivity.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        //
+        menuInflater.inflate(R.menu.main, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
