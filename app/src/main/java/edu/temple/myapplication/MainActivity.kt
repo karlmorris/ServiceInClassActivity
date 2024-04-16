d67db2ecd8af5fd9c55878cc5f63771902df7ba3
Jonathan Augustin <tun45218@temple.edu>
2024-04-16 12:06:55 -0400

    Menu items will now alert the user; buttons will not.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,15 +32,15 @@ class MainActivity : AppCompatActivity() {
             serviceConnection, BIND_AUTO_CREATE
         )
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        findViewById<Button>(R.id.actionStart).setOnClickListener {
             timerBinder?.start(100)
         }
 
-        findViewById<Button>(R.id.pauseButton).setOnClickListener {
+        findViewById<Button>(R.id.actionPause).setOnClickListener {
             timerBinder?.pause()
         }
         
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
+        findViewById<Button>(R.id.actionStop).setOnClickListener {
             timerBinder?.stop()
         }
     }
@@ -46,6 +48,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.actionStart -> {
+                Toast.makeText(this, "Timer is running", Toast.LENGTH_SHORT).show()
+                timerBinder?.start(100)
+            }
+
+            R.id.actionPause -> {
+                Toast.makeText(this, "Timer is on hold", Toast.LENGTH_SHORT).show()
+                timerBinder?.pause()
+            }
+
+            R.id.actionStop -> {
+                Toast.makeText(this, "Timer is off", Toast.LENGTH_SHORT).show()
+                timerBinder?.stop()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroy() {
